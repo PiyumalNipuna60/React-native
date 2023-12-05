1d04d7c8b7d889adcd56ac720e656be8e8040c2d
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-12-05 13:00:15 +0530

    inline and another style add content

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View, Button } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hy piyumal</Text>
-      <Text>hy piyumal</Text>
+      <Text style={styles.dummy}>hy piyumal</Text>
+      <Text style={{margin:16, borderWidth:2, borderColor:'red', color:'green'}}>hy piyumal</Text>
       <StatusBar style="auto" />
       <Button title="Learn More" color="#841584" />
     </View>
